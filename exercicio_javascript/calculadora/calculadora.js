1fd9f80b7c558828b015102f1f7103402663bc74
Pedro Augusto <pedroaugusto.cg19@gmail.com>
2023-06-06 08:42:29 -0300

    Exercícios javascript-8

---
+++ exercicio_javascript/calculadora/calculadora.js
@@ -0,0 +1,33 @@
+
+
+function calcular(tipo, valor) { 
+
+    if (tipo === 'acao') {
+
+        if(valor === 'c') {
+            //limpar resultado
+
+            document.getElementById('resultado').value = ''
+        }
+
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+
+            document.getElementById('resultado').value += valor
+        }
+
+        if(valor === '=') {
+            var valor_campo = eval(document.getElementById('resultado').value)
+
+            document.getElementById('resultado').value = valor_campo
+            console.log(eval(valor_campo))
+        }
+
+    } else if(tipo === 'valor') {
+        
+        document.getElementById('resultado').value += valor 
+    
+    }
+
+
+}
+    
