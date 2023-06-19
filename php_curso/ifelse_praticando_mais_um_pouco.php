3077ce47a7fc35cb83a8e9d500343b71453ab735
Pedro Augusto <pedroaugusto.cg19@gmail.com>
2023-06-19 14:16:08 -0300

    praticando legalzinho

--- php_curso/ifelse_praticando_mais_um_pouco.php
+++ php_curso/ifelse_praticando_mais_um_pouco.php
@@ -12,22 +12,22 @@
     <?php
 
         $usuario_possui_cartao_loja = true;
-        $valor_compra = 225;
+        $valor_compra = 70;
         
         $valor_frete = 50;
-        $recebeu_desconto_frete = false;
+        $recebeu_desconto_frete = true;
 
         if($usuario_possui_cartao_loja == true && $valor_compra >= 400) {
             $valor_frete = 0;
-            $recebeu_desconto_frete = true;
 
         }else if($usuario_possui_cartao_loja && $valor_compra >= 300) {
             $valor_frete = 10;
-            $recebeu_desconto_frete = true;
 
         }else if($usuario_possui_cartao_loja && $valor_compra >= 100) {
-            $valor_frete = 25;
-            $recebeu_desconto_frete = true;
+            $valor_frete = 25; 
+
+        }else {
+            $recebeu_desconto_frete = false;
         }
 
     ?>
