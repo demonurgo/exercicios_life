5b00ad640899323af4d34abd8b7265efe0b4719b
Pedro Augusto <pedroaugusto.cg19@gmail.com>
2023-06-26 09:57:17 -0300

    php-OO-ABSTRAÇÃO

---
+++ php_curso/php_oo/OO_abstrção.php
@@ -0,0 +1,52 @@
+<?php
+
+    //modelo
+    class Funcionario {
+        //atributos
+        public $nome = '(nome)';
+        public $telefone = '(telefone)';
+        public $numFilhos = '(filhos)';
+
+        //metodos
+        function resumirCadFunc(){
+            return "$this->nome tem $this->numFilhos filho(s) e seu telefone é $this->telefone.";
+        }
+
+        function modificarNumFilhos($numFilhos){
+            //afetar atributo do objeto;
+            $this->numFilhos = $numFilhos;
+        }
+
+        function modificarNome($nome){
+            $this->nome = $nome;
+        }
+        
+        function modificarTelefone($telefone){
+            $this->telefone = $telefone;
+        }
+
+    }
+
+    $y = new Funcionario();
+    echo $y->resumirCadFunc();
+    echo '<br>';
+    
+    echo '<h2>Funcionário Maria</h2>';
+    $y->modificarNome('Maria');
+    $y->modificarTelefone('81 99615-1072');
+    $y->modificarNumFilhos(4);
+
+    echo $y->resumirCadFunc();
+    echo '<hr>';
+
+    $x = new Funcionario();
+    echo $x->resumirCadFunc();
+    echo '<br>';
+
+    echo '<h2>Funcionário José</h2>';
+    $x->modificarNome('José');
+    $x->modificarTelefone('81 98812-0041');
+    $x->modificarNumFilhos(5);
+
+    echo $x->resumirCadFunc();
+?>
